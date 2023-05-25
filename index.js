edb3f548f4bbd8fd769ac01566cea908830cad26
MehediHasan95 <imehedihasan95@gmail.com>
2023-05-25 21:49:37 +0600

    chef data load and send to client site

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const chefrecipes = require("./data/chef-recipes.json");
+
 app.get("/", (req, res) => {
   res.send("Cusine Craft Server is running");
 });
+
+app.get("/v1/chefrecipes", (req, res) => res.send(chefrecipes));
 
 app.listen(port, () => console.log("Server is running port: ", port));
